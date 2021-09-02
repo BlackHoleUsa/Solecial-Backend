4ae19b6c1ad296b7939873fbf0726555221c4e0b
Agha Ali Abbas <agha.ali@codingpixel.com>
2021-09-02 15:31:21 +0500

    roles are set now

--- src/routes/v1/collection.route.js
+++ src/routes/v1/collection.route.js
@@ -2,9 +2,10 @@ const express = require('express');
 const validate = require('../../middlewares/validate');
 const collectionController = require('../../controllers/collection.controller');
 const { createCollectionVS } = require('../../validations/collection.validation');
+const auth = require('../../middlewares/auth');
 
 const router = express.Router();
 
-router.post('/createCollection', validate(createCollectionVS), collectionController.createCollection);
+router.post('/createCollection', [auth('consumedByArtistOnly'), validate(createCollectionVS)], collectionController.createCollection);
 
 module.exports = router;
