73ed4ea1e42d1dd893a1cbeed7921f82aa4dd878
Muhammad Waqar <muhammadwaqar.cp@gmail.com>
2021-09-08 13:11:50 +0500

    place bid api's done

--- src/triggers/triggers.js
+++ src/triggers/triggers.js
@@ -5,3 +5,5 @@ const LISTENERS = require('../controllers/listeners.controller');
 EVENT.addListener('add-collection-in-user', LISTENERS.addCollectionInUser);
 EVENT.addListener('add-artwork-in-user', LISTENERS.addArtworkInUser);
 EVENT.addListener('add-artwork-in-collection', LISTENERS.addArtworkInCollection);
+
+EVENT.addListener('save-bid-in-artwork', LISTENERS.saveBidInArtwork);
