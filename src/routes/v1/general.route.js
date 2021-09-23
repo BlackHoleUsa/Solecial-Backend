07eb3a75ade7ec12fc0c289892b4403fb0b32b88
Muhammad Waqar <muhammadwaqar.cp@gmail.com>
2021-09-23 11:56:47 +0500

    changed api's to live

--- src/routes/v1/general.route.js
+++ src/routes/v1/general.route.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const { generalController } = require('../../controllers');
 const validate = require('../../middlewares/validate');
-// const auctionController = require('../../controllers/auction.controller');
-const { auctionValidation, generalValidation } = require('../../validations');
+const { generalValidation } = require('../../validations');
 
 const router = express.Router();
 
