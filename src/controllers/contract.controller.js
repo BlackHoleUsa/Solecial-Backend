038bc0eec344325fcfa3cbcb5d6f5640add8c0ed
shamoon1997 <sshamoon83@gmail.com>
2021-12-28 13:13:44 +0500

    fix

--- src/controllers/contract.controller.js
+++ src/controllers/contract.controller.js
@@ -32,18 +32,18 @@ const updateCollectionAddress = async (tokenId, owner, colName) => {
   console.log('artwork token id updated successfully');
 };
 const transfer = async (transferContract) => {
-  const { from, to, tokenId } = transferContract;
-  try {
-    const artwork = await Artwork.findOne({ tokenId: tokenId });
-    await User.findOneAndUpdate({ _id: artwork._id }, { $pull: { artworks: artwork._id } });
-    await Auction.findOneAndDelete({ artwork: artwork._id });
-    await BuySell.findOneAndDelete({ artwork: artwork._id });
-    await Artwork.findOneAndDelete({ _id: artwork._id });
-    console.log('transfer event called');
-  }
-  catch (error) {
-    console.log(error);
-  }
+  // const { from, to, tokenId } = transferContract;
+  // try {
+  //   const artwork = await Artwork.findOne({ tokenId: tokenId });
+  //   await User.findOneAndUpdate({ _id: artwork._id }, { $pull: { artworks: artwork._id } });
+  //   await Auction.findOneAndDelete({ artwork: artwork._id });
+  //   await BuySell.findOneAndDelete({ artwork: artwork._id });
+  //   await Artwork.findOneAndDelete({ _id: artwork._id });
+  //   console.log('transfer event called');
+  // }
+  // catch (error) {
+  //   console.log(error);
+  // }
 
 };
 const handleNewAuction = async (saleFromContract) => {
